138b6d0e78fe104dc8c6e029881cf884984d5843
1GodLess5 <gacekalbert@seznam.cz>
2024-01-11 22:30:26 +0100

    Created playing field, Created void drawField - draws the field in console

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
+        int[][] field = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+        };
 
+        drawField(field);
+
+
+
+    }
+
+    public static void drawField(int[][] field){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (field[i][j] == 0) {
+                    if (j == 2){
+                        System.out.print(" ");
+                    }else{
+                        System.out.print(" |");
+                    }
+                } else if (field[i][j] == 1) {
+                    if (j == 2){
+                        System.out.print("0");
+                    }else{
+                        System.out.print("0|");
+                    }
+                } else {
+                    if (j == 2){
+                        System.out.print("X");
+                    }else{
+                        System.out.print("X|");
+                    }
+                }
+            }
+            if (i != 2){
+                System.out.println();
+                System.out.print("-----");
+                System.out.println();
+            }
+        }
     }
 }
